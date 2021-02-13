eb4b182f773d0286fbf57df23dfacba8ea4e481e
Kayen <fadeew.k@mail.ru>
2021-02-13 23:04:25 +0300

    Homework 4

--- hw04/src/main/java/homework/CustomerService.java
+++ hw04/src/main/java/homework/CustomerService.java
@@ -1,23 +1,36 @@
 package homework;
 
 
+import java.util.Collections;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class CustomerService {
 
-    //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    NavigableMap<Customer, String> map = new TreeMap<>();
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> entry = map.firstEntry();
+        if(entry != null){
+            return Collections.singletonMap(new Customer(entry.getKey()), entry.getValue()).entrySet().iterator().next();
+        } else {
+            return null;
+        }
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        Map.Entry<Customer, String> entry = map.higherEntry(customer);
+        if(entry != null) {
+            return Collections.singletonMap(new Customer(entry.getKey()), entry.getValue()).entrySet().iterator().next();
+        } else {
+            return null;
+        }
     }
 
     public void add(Customer customer, String data) {
-
+        map.put(customer, data);
     }
 }
